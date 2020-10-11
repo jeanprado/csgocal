022af8e92c2fb7d606aa0e84ae9c0d3f951bf0b7
Jean Prado <jeangrprado@gmail.com>
2020-10-11 01:46:56 -0300

    creates google calendar csv

---
+++ gcal.R
@@ -0,0 +1,23 @@
+source('scraping.R')
+
+gcal <- matches %>%
+  mutate(gcal_title=ifelse(has_match == T,
+                           paste0(team1, ' x ', team2, ' (★', rating, '/5)'),
+                           match_type),
+         gcal_start_date=format(datetime, "%D"),
+         gcal_start_time=format(datetime, '%R'),
+         gcal_end_date=format(datetime + 60 * 90, "%D"),
+         gcal_end_time=format(datetime + 60 * 90, '%R'),
+         gcal_desc=paste0('https://www.twitch.tv/gaules\nhttps://www.twitch.tv/esl_csgo\n\n', match_url),
+         gcal_all_day_event='False', gcal_private='False',
+         gcal_location=ifelse(is.na(match_type), event,
+                              'TBD')) %>% select(starts_with('gcal_')) %>% 
+  select(Subject=gcal_title,
+         `Start Date`=gcal_start_date,
+         `Start Time`=gcal_start_time,
+         `End Date`=gcal_end_date,
+         `End Time`=gcal_end_time,
+         `All Day Event`=gcal_all_day_event,
+         Description=gcal_desc,
+         Location=gcal_location,
+         Private=gcal_private) %>% write_csv('data/gcal.csv')
